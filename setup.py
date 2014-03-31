cfadc185a71687bdf34422c9b8141cad70e9c538
David Parlevliet <david@reactordigital.com.au>
2014-03-31 13:29:00 +1100

    pypi setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+import os
+from setuptools import setup
+
+setup(
+  name="rwrapper",
+  version="1.0.1",
+  description="This package provides an ORM layer for RethinkDB",
+  url="https://github.com/dparlevliet/rwrapper",
+  maintainer="dparlevliet",
+  packages=['rwrapper'],
+  install_requires=['rethinkdb']
+)
